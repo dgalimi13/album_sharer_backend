6ac271ec957df27fe98303bb71f7a3deab64f8bd
danny galimi <dannygalimi13@aim.com>
2022-02-04 16:03:01 -0500

    added to DOMContentLoaded function

--- album_sharer_frontend/index.js
+++ album_sharer_frontend/index.js
@@ -1,3 +1,9 @@
+const endPoint = "http://127.0.0.1:3000/api/v1/albums"
+
 document.addEventListener('DOMContentLoaded', () => {
-    console.log("loaded");
+    fetch(endPoint)
+    .then(response => response.json())
+    .then (albums => {
+        console.log(albums);
+    })
 })
