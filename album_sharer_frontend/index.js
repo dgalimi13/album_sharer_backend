9b7212839ffecaeed760f2e01095f7a743903dcf
danny galimi <dannygalimi13@aim.com>
2022-02-04 16:54:09 -0500

    created get albums function in index.js, added it to DOMContentLoaded function as callback

--- album_sharer_frontend/index.js
+++ album_sharer_frontend/index.js
@@ -1,9 +1,23 @@
 const endPoint = "http://127.0.0.1:3000/api/v1/albums"
 
 document.addEventListener('DOMContentLoaded', () => {
+    getAlbums()
+})
+
+function getAlbums() {
     fetch(endPoint)
     .then(response => response.json())
-    .then (albums => {
-        console.log(albums);
+    .then(albums => {
+        albums.data.forEach(album => {
+            const albumMarkup = `
+            <div data-id=${album.id}>
+            <p>${album.attributes.name}</p>
+            <h3>${album.attributes.artist}</h3>
+            <p>${album.attributes.genre.name}</p>
+            </div>
+            <br><br>`;
+
+            document.querySelector('#album-container').innerHTML += albumMarkup
+        })
     })
-})
+}
