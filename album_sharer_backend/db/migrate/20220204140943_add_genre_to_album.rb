b88d8203ee900c117a23da03408b9660227fd72c
danny galimi <dannygalimi13@aim.com>
2022-02-04 15:10:36 -0500

    changed project structure

--- album_sharer_backend/db/migrate/20220204140943_add_genre_to_album.rb
+++ album_sharer_backend/db/migrate/20220204140943_add_genre_to_album.rb
@@ -1,5 +1,5 @@
 class AddGenreToAlbum < ActiveRecord::Migration[6.1]
   def change
-    add_reference :albums, :genre, foreign_key: true
+    add_reference :albums, :genre, null: false, foreign_key: true
   end
 end
--- album_sharer_backend/db/schema.rb
+++ album_sharer_backend/db/schema.rb
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2022_02_04_140943) do
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "genre_id"
+    t.bigint "genre_id", null: false
     t.index ["genre_id"], name: "index_albums_on_genre_id"
   end
 
