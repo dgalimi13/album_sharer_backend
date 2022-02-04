79e4640f22a5c357adf16d195a06b1abacc66197
danny galimi <dannygalimi13@aim.com>
2022-02-04 09:46:06 -0500

    removed instance variables from index action in ablums controller

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::AlbumsController < ApplicationController
 
     def index
-        @albums = Album.all
-        render json: @albums
+        albums = Album.all
+        render json: albums
     end 
 end
