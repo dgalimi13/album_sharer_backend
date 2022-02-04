1c2f566e8db38247aec8130232d3e9db4bab0e97
danny galimi <dannygalimi13@aim.com>
2022-02-04 10:01:46 -0500

    added create action and strong params to albums controller

--- app/controllers/api/v1/albums_controller.rb
+++ app/controllers/api/v1/albums_controller.rb
@@ -4,4 +4,15 @@ class Api::V1::AlbumsController < ApplicationController
         albums = Album.all
         render json: albums
     end 
+
+    def create
+        album = Album.new(album_params)
+    end 
+
+    private
+
+    def album_params
+        params.require(:album).permit(:name, :artist, :description, :genre_id)
+    end 
+
 end
