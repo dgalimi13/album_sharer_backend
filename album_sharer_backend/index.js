2aa55b0b55ca0fe93cd812a6d9fd87ede2132913
danny galimi <dannygalimi13@aim.com>
2022-02-05 13:55:52 -0500

    changed project structure

--- album_sharer_backend/index.js
+++ album_sharer_backend/index.js
@@ -3,9 +3,10 @@ const endPoint = "http://127.0.0.1:3000/api/v1/albums"
 document.addEventListener('DOMContentLoaded', () => {
     getAlbums()
 
-    const createAlbumForm = document.querySelector("create-album-form")
+    const createAlbumForm = document.querySelector("#create-album-form")
 
-    createAlbumForm.addEventListener("submit", (e) => console.log(e))
+    createAlbumForm.addEventListener("submit", (e) => 
+    createFormHandler(e))
 })
 
 function getAlbums() {
@@ -24,4 +25,13 @@ function getAlbums() {
             document.querySelector('#album-container').innerHTML += albumMarkup
         })
     })
+}
+
+function createFormHandler(e) {
+    e.preventDefault()
+    const nameInput = document.querySelector('#input-name').value 
+    const descriptionInput = document.querySelector('#input-description').value
+    const artistInput = document.querySelector('#input-artist').value 
+    const GenreInput = document.querySelector('#genres').value
+
 }
